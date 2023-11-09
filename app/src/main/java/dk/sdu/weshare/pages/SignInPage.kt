5c387398bf06cf7db0cb0a39878ef649cc1a2aee
jonfr <jojak19@student.sdu.dk>
2023-11-09 15:56:04 +0100

    Login screen

--- app/src/main/java/dk/sdu/weshare/pages/SignInPage.kt
+++ app/src/main/java/dk/sdu/weshare/pages/SignInPage.kt
@@ -7,12 +7,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -20,15 +27,39 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignInPage(onSignIn: () -> Unit) {
+    var username by remember { mutableStateOf("The username should go here") }
+    var password by remember { mutableStateOf("And the password should be here") }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight()) {
-        Text("Sign In", fontSize = 70.sp)
+            .fillMaxHeight()
+            .padding(48.dp)) {
+        Text("UOMI", fontSize = 70.sp)
+        Spacer(Modifier.size(48.dp))
+        OutlinedTextField(
+            username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        )
+
+        OutlinedTextField(
+            password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        )
+
         Spacer(Modifier.size(48.dp))
         Button(
             onClick = onSignIn,
@@ -37,12 +68,17 @@ fun SignInPage(onSignIn: () -> Unit) {
                 contentColor = Color.Black
             ),
             modifier = Modifier
+                .fillMaxWidth()
                 .border(
                     BorderStroke(2.dp, Color.Black),
                     CircleShape
                 )
                 .clip(CircleShape)) {
-            Text("Sign in with google", fontSize = 30.sp)
+            Text("Sign in", fontSize = 30.sp)
+
         }
     }
+
+
+
 }
