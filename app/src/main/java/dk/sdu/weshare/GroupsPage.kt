bbc87789bd5e3a06642805bf989be2c22c6c1d46
jonfr <jojak19@student.sdu.dk>
2023-11-09 15:31:46 +0100

    Added groupsPage

--- app/src/main/java/dk/sdu/weshare/GroupsPage.kt
+++ app/src/main/java/dk/sdu/weshare/GroupsPage.kt
@@ -1,16 +1,29 @@
 package dk.sdu.weshare
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.KeyboardArrowRight
+import androidx.compose.material.icons.twotone.Add
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -20,24 +33,73 @@ fun GroupsPage(
     onViewGroup: (Int) -> Unit,
     onCreateGroup: () -> Unit,
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()) {
-        Text("Groups", fontSize = 70.sp)
-        Spacer(Modifier.size(48.dp))
-        Button(onClick = { onViewProfile(6969) }) {
-            Text("onViewProfile", fontSize = 30.sp)
+    Column {
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.AccountCircle,
+                contentDescription = "Profile",
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
+                    .clickable { onViewProfile(6969) }
+            )
+            Text("user.name", fontSize = 30.sp)
+
+            Icon(
+                imageVector = Icons.TwoTone.Add,
+                contentDescription = "onCreateGroup",
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
+                    .clickable { onCreateGroup() }
+            )
         }
-        Spacer(Modifier.size(16.dp))
-        Button(onClick = { onViewGroup(64) }) {
-            Text("onViewGroup", fontSize = 30.sp)
-        }
-        Spacer(Modifier.size(16.dp))
-        Button(onClick = onCreateGroup) {
-            Text("onCreateGroup", fontSize = 30.sp)
+
+
+        Column(
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier
+                .padding(32.dp)
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+
+            Text("Groups", fontSize = 36.sp, fontWeight = FontWeight.Bold)
+
+            Spacer(Modifier.size(16.dp))
+
+            Column {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                    .fillMaxWidth()
+                    .border(1.dp, color = Color.Black)
+                    .padding(start = 16.dp)
+                    .clickable { onViewGroup(1)
+                    }
+                ) {
+                    Text("Group 1", fontSize = 30.sp)
+
+                    Icon(
+                        imageVector = Icons.Outlined.KeyboardArrowRight,
+                        contentDescription = "onViewGroup",
+                        modifier = Modifier
+                            .size(60.dp)
+                            .clip(CircleShape)
+
+                    )
+                }
+
+            }
         }
     }
 }
+
+
