9518fedf193c91276852195df4c7f7d4e9280268
jonfr <jojak19@student.sdu.dk>
2023-11-11 17:04:19 +0100

    added dependencies and gave acces to internet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,13 @@ android {
 dependencies {
     val navVersion = "2.7.4"
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
+//Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+//Coil you can also use Glide in this case
+    implementation ("com.google.accompanist:accompanist-coil:0.x.x")
+//Paging 3.0
+    implementation ("Androidx.Paging:Paging-compose:1.x.x")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
