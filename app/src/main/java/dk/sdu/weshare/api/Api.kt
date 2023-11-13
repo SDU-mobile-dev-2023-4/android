27a4f00eba6c726a16c32008bbbbe83da5ff66d0
jonfr <jojak19@student.sdu.dk>
2023-11-13 13:03:14 +0100

    added bearer token to header

--- app/src/main/java/dk/sdu/weshare/api/Api.kt
+++ app/src/main/java/dk/sdu/weshare/api/Api.kt
@@ -4,12 +4,10 @@ import dk.sdu.weshare.models.LoginCredentials
 import dk.sdu.weshare.models.User
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface Api {
 
-	@Headers("Accept: application/json", "Content-Type: application/json")
 	@POST("/api/login")
 	fun login(@Body loginRequest: LoginCredentials): Call<User>
 }
