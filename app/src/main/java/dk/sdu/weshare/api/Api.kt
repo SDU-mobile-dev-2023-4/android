9d9904da41ab3747baed5612b986494c3db1d89c
jakob753951 <jlm1999@live.dk>
2023-11-13 20:30:43 +0100

    Huge update to the APIs. Might even work now :o

--- app/src/main/java/dk/sdu/weshare/api/Api.kt
+++ app/src/main/java/dk/sdu/weshare/api/Api.kt
@@ -2,7 +2,6 @@ package dk.sdu.weshare.api
 
 import dk.sdu.weshare.models.Expense
 import dk.sdu.weshare.models.Group
-import dk.sdu.weshare.models.LoginCredentials
 import dk.sdu.weshare.models.User
 import retrofit2.Call
 import retrofit2.http.Body
@@ -11,37 +10,36 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface Api {
+	@POST("api/groups/{id}/expenses")
+	fun addExpenseToGroup(@Path("id") groupId: Int, @Body expense: Expense): Call<Group>
 
-	@POST("api/groups/{groupId}/expenses")
-	fun addExpenseToGroup(@Path("groupId") groupId: Int, @Body expense: Expense): Call<Expense>
+	@POST("api/groups/{id}/users")
+	fun addUserToGroup(@Path("id") groupId: Int, @Query("email") userEmail: String): Call<Group>
 
-	@POST("api/groups/{groupId}/users")
-	fun addUserToGroup(@Path("groupId") groupId: Int, @Body user: User): Call<User>
+	@DELETE("api/groups/{id}/users")
+	fun removeUserFromGroup(@Path("id") groupId: Int, @Query("email") userEmail: String): Call<Group>
 
-	@DELETE("api/groups/{groupId}/users")
-	fun removeUserFromGroup(@Path("groupId") groupId: Int, @Body user: User): Call<User>
+	@GET("api/groups/{id}")
+	fun getGroup(@Path("id") groupId: Int): Call<Group>
 
-	@GET("api/groups/{groupId}/users")
-	fun getGroup(@Path("groupId") groupId: Int, @Body user: User): Call<User>
+	@PUT("api/groups/{id}")
+	fun updateGroup(@Path("id") groupId: Int, @Query("name") name: String): Call<Group>
 
-	@PUT("api/groups/{groupId}/users")
-	fun updateGroup(@Path("groupId") groupId: Int, @Body user: User): Call<User>
-
-	@DELETE("api/groups/{groupId}")
-	fun deleteGroup(@Body group: Group): Call<Group>
+	@DELETE("api/groups/{id}")
+	fun deleteGroup(@Path("id") groupId: Int): Call<Error>
 
 	@GET("api/groups")
-	fun getAllGroups(@Body group: Group): Call<Group>
+	fun getAllGroups(): Call<List<Group>>
 
 	@POST("api/groups")
-	fun createGroup(@Body group: Group): Call<Group>
+	fun createGroup(@Query("name") name: String): Call<Group>
 
 	@POST("/api/register")
-	fun register(@Body registerRequest: LoginCredentials): Call<User>
+	fun register(@Query("name") name: String, @Query("email") email: String, @Query("password") password: String, @Query("device_name") deviceName: String): Call<User>
 
 	@POST("/api/login")
-	fun login(@Body loginRequest: LoginCredentials): Call<User>
-
+	fun login(@Query("email") email: String, @Query("password") password: String, @Query("device_name") deviceName: String): Call<User>
 }
--- app/src/main/java/dk/sdu/weshare/authentication/Auth.kt
+++ app/src/main/java/dk/sdu/weshare/authentication/Auth.kt
@@ -1,23 +1,25 @@
 package dk.sdu.weshare.authentication
 
 import dk.sdu.weshare.api.ApiService
-import dk.sdu.weshare.models.LoginCredentials
 import dk.sdu.weshare.models.User
 
 class Auth {
 	companion object {
-		private var _user: User? = null
-		var user: User?
-			get() { return _user }
-			private set(value) { _user = value }
+		var user: User? = null
+			private set
+	}
+
+	fun register(name: String, email: String, password: String, onResult: (User?) -> Unit) {
+		ApiService.register(name, email, password) {
+			if (it != null) {
+				user = it
+			}
+			onResult(it)
+		}
 	}
 
 	fun login(email: String, password: String, onResult: (User?) -> Unit) {
-		println("called login() method")
-		val apiService = ApiService()
-		val loginRequest = LoginCredentials(email, password)
-		apiService.login(loginRequest) {
-			println("callback")
+		ApiService.login(email, password) {
 			if (it != null) {
 				user = it
 			}
