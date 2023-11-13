4accce441f74a87bff0febf3ce3cd838a285b3f6
Mikkel Albrechtsen <mikkel.alb@gmail.com>
2023-11-13 22:26:13 +0100

    Removed unused import

--- app/src/main/java/dk/sdu/weshare/pages/ProfilePage.kt
+++ app/src/main/java/dk/sdu/weshare/pages/ProfilePage.kt
@@ -14,7 +14,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
