e12fdc2fa328164ec4997f482b2fa882dd569563
jonfr <jojak19@student.sdu.dk>
2023-11-24 08:50:44 +0100

    this is no longer used to remove a user

--- app/src/main/java/dk/sdu/weshare/api/requests/RemoveUserFromGroupRequest.kt
+++
@@ -1,7 +0,0 @@
-package dk.sdu.weshare.api.requests
-
-import com.google.gson.annotations.SerializedName
-
-data class RemoveUserFromGroupRequest(
-    @SerializedName("email") val userEmail: String,
-)
